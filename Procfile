790953034423a01265421e2c2cd6dbeba6983b64
johndoe-error <66063257+johndoe-error@users.noreply.github.com>
2020-05-28 14:56:31 +0300

    PyTelegramDev for Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python3 pytelegramdev_git.py
---
+++ pytelegramdev_git.py
@@ -0,0 +1,42 @@
+import telebot, os
+
+token = str(os.environ.get('BOT_TOKEN'))
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Привет, ' + message.from_user.first_name + '!\n\nЯ бот, который поможет тебе в программировании своих собственных Телеграм-ботов!\n\nВот список моих команд:\n\n/json\n/info\n/posts\n\nА еще ты можешь отправить мне любой файл, чтобы получить информацию о нем!\n\nУдачной разработки!')
+
+@bot.message_handler(commands=['json'])
+def json(message):
+    bot.send_message(message.chat.id, message)
+
+@bot.message_handler(commands=['info'])
+def info(message):
+    bot.send_message(message.chat.id, '👨‍💻' + message.from_user.first_name + ' ' + message.from_user.last_name + '\n' + '📢' + '@' + message.from_user.username + '\n'+ '🎟' + str(message.from_user.id) + '\n' + '💬' + str(message.chat.id)) 
+
+@bot.message_handler(commands=['posts'])
+def posts(message):
+    bot.send_message(message.chat.id, 'Наш канал: @ENTER_ERROR\n1 пост: https://telegra.ph/Kurs-po-sozdaniyu-Telegram-botov-ot-John-Doe-CHast-1-05-09\n2 пост: https://telegra.ph/Kurs-po-sozdaniyu-Telegram-botov-ot-John-Doe-CHast-2-05-09\nЗ пост:https://telegra.ph/Kurs-po-sozdaniyu-Telegram-botov-ot-John-Doe-CHast-3-05-10\n4 пост: https://telegra.ph/Kurs-po-sozdaniyu-Telegram-botov-ot-John-Doe-CHast-4-05-14')
+
+@bot.message_handler(content_types=['photo'])
+def photo(message):
+    bot.send_message(message.chat.id, message.photo)
+
+@bot.message_handler(content_types=['sticker'])
+def sticker(message):
+    bot.send_message(message.chat.id, message.sticker)
+
+@bot.message_handler(content_types=['video'])
+def video(message):
+    bot.send_message(message.chat.id, message.video)
+
+@bot.message_handler(content_types=['audio'])
+def audio(message):
+    bot.send_message(message.chat.id, message.audio)
+
+@bot.message_handler(content_types=['document'])
+def document(message):
+    bot.send_message(message.chat.id, message.document)
+
+bot.polling()
